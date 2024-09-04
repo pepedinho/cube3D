38666baaca62ce46607971b520284d975c295c1e
JudaNana <moussaadamou898@gmail.com>
2024-09-04 16:48:10 +0200

    commit for merge imad

--- srcs/raycasting/minimap/render_map.c
+++ srcs/raycasting/minimap/render_map.c
@@ -6,7 +6,7 @@
 /*   By: madamou <madamou@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/03 20:00:33 by itahri            #+#    #+#             */
-/*   Updated: 2024/09/04 16:46:59 by madamou          ###   ########.fr       */
+/*   Updated: 2024/09/04 16:47:50 by madamou          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,9 @@ void	render_map(t_map_data *map_data)
 {
 	int		x;
 	int		y;
-	int len_x;
-	int len_y;
 	char	**map;
 
 	y = 0;
-	len_x = len_bigest_line(map_data->map) / 2;
-	len_y = ft_strlen_2d(map_data->map) / 2;
 	map = map_data->map;
 	while (map[y])
 	{
@@ -35,8 +31,8 @@ void	render_map(t_map_data *map_data)
 					* HEIGHT);
 			else if (map[y][x] == '0')
 				mlx_put_image_to_window(map_data->mlx->init,
-					map_data->mlx->window, map_data->mlx->black_i, x * WIDTH / len_x, y
-					* HEIGHT / len_y);
+					map_data->mlx->window, map_data->mlx->black_i, x * WIDTH, y
+					* HEIGHT);
 			x++;
 		}
 		y++;
