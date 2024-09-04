9291f2109523f2dfe5c4bdc37091c30f3b5cb78a
pepedinho <2spii94@gmail.com>
2024-09-04 16:17:54 +0200

    first DDA implementation for raycasting (not viable at all)

--- srcs/raycasting/minimap/render_map.c
+++ srcs/raycasting/minimap/render_map.c
@@ -12,39 +12,6 @@
 
 #include "../../../includes/cube3D.h"
 
-void	trace(double s_x, double s_y, double e_x, double e_y,
-		t_map_data *map_data, int color)
-{
-	double	x_step;
-	double	y_step;
-
-	// printf("debug : start x : %f, start y : %f | to x : %f, to y : %f\n",
-	//	s_x, s_y, e_x, e_y);
-	x_step = abs_value((e_x - s_x) / 100);
-	y_step = abs_value((e_y - s_y) / 100);
-	if (s_x < e_x && s_y < e_y)
-	{
-		// printf("xstep == %f, ystep == %f\n", x_step, y_step);
-		while (s_x < e_x && s_y < e_y)
-		{
-			s_x += x_step;
-			s_y += y_step;
-			mlx_pixel_put(map_data->mlx->init, map_data->mlx->window, s_x * 64,
-				s_y * 64, color);
-		}
-	}
-	else if (s_x > e_x && s_y > e_y)
-	{
-		while (s_x > e_x || s_y > e_y)
-		{
-			mlx_pixel_put(map_data->mlx->init, map_data->mlx->window, s_x * 64,
-				s_y * 64, color);
-			s_x -= x_step;
-			s_y -= y_step;
-		}
-	}
-}
-
 void	render_map(t_map_data *map_data)
 {
 	int		x;
@@ -58,11 +25,11 @@ void	render_map(t_map_data *map_data)
 		x = 0;
 		while (map[y][x])
 		{
-			if (map[y][x] == '1' && x > 0)
+			if (map[y][x] == '1')
 				mlx_put_image_to_window(map_data->mlx->init,
 					map_data->mlx->window, map_data->mlx->white_i, x * WIDTH, y
 					* HEIGHT);
-			else if (map[y][x] == '0' && x > 0)
+			else if (map[y][x] == '0')
 				mlx_put_image_to_window(map_data->mlx->init,
 					map_data->mlx->window, map_data->mlx->black_i, x * WIDTH, y
 					* HEIGHT);
