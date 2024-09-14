52072f240397d10d2aca1fa561d5bf831f0d6c1e
OppaiFun <moussaadamou898@gmail.com>
2024-09-14 16:21:49 +0200

    change player move check

--- srcs/mlx/move/player_move.c
+++ srcs/mlx/move/player_move.c
@@ -6,7 +6,7 @@
 /*   By: madamou <madamou@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/08 12:25:58 by itahri            #+#    #+#             */
-/*   Updated: 2024/09/14 00:36:46 by madamou          ###   ########.fr       */
+/*   Updated: 2024/09/14 16:18:00 by madamou          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,57 +14,41 @@
 
 void	right(t_map_data *data, double move_speed)
 {
-	if (data->map[(int)(data->p_pos.r_y)][(int)(data->p_pos.r_x
-			+ data->p_pos.plane_x * move_speed)] != '1'
-		&& data->map[(int)(data->p_pos.r_y)][(int)(data->p_pos.r_x
-			+ data->p_pos.plane_x * move_speed)] != 'D')
+	if (!ft_is_in_charset(data->map[(int)(data->p_pos.r_y)][(int)(data->p_pos.r_x
+			+ data->p_pos.plane_x * move_speed)], "1D"))
 		data->p_pos.r_x += data->p_pos.plane_x * move_speed;
-	if (data->map[(int)(data->p_pos.r_y + data->p_pos.plane_y
-			* move_speed)][(int)(data->p_pos.r_x)] != '1'
-		&& data->map[(int)(data->p_pos.r_y + data->p_pos.plane_y
-			* move_speed)][(int)(data->p_pos.r_x)] != 'D')
+	if (!ft_is_in_charset(data->map[(int)(data->p_pos.r_y + data->p_pos.plane_y
+			* move_speed)][(int)(data->p_pos.r_x)], "1D"))
 		data->p_pos.r_y += data->p_pos.plane_y * move_speed;
 }
 
 void	left(t_map_data *data, double move_speed)
 {
-	if (data->map[(int)(data->p_pos.r_y)][(int)(data->p_pos.r_x
-			- data->p_pos.plane_x * move_speed)] != '1'
-		&& data->map[(int)(data->p_pos.r_y)][(int)(data->p_pos.r_x
-			- data->p_pos.plane_x * move_speed)] != 'D')
+	if (!ft_is_in_charset(data->map[(int)(data->p_pos.r_y)][(int)(data->p_pos.r_x
+			- data->p_pos.plane_x * move_speed)], "1D"))
 		data->p_pos.r_x -= data->p_pos.plane_x * move_speed;
-	if (data->map[(int)(data->p_pos.r_y - data->p_pos.plane_y
-			* move_speed)][(int)(data->p_pos.r_x)] != '1'
-		&& data->map[(int)(data->p_pos.r_y - data->p_pos.plane_y
-			* move_speed)][(int)(data->p_pos.r_x)] != 'D')
+	if (!ft_is_in_charset(data->map[(int)(data->p_pos.r_y - data->p_pos.plane_y
+			* move_speed)][(int)(data->p_pos.r_x)], "1D"))
 		data->p_pos.r_y -= data->p_pos.plane_y * move_speed;
 }
 
 void forward(t_map_data *data, double move_speed)
 {
-    if (data->map[(int)(data->p_pos.r_y)][(int)(data->p_pos.r_x
-		+ data->p_pos.dir_x * move_speed)] != '1'
-        && data->map[(int)(data->p_pos.r_y)][(int)(data->p_pos.r_x
-		+ data->p_pos.dir_x * move_speed)] != 'D')
-        data->p_pos.r_x += data->p_pos.dir_x * move_speed;
-    if (data->map[(int)(data->p_pos.r_y + data->p_pos.dir_y
-		* move_speed)][(int)(data->p_pos.r_x)] != '1'
-        && data->map[(int)(data->p_pos.r_y + data->p_pos.dir_y
-		* move_speed)][(int)(data->p_pos.r_x)] != 'D')
+	if (!ft_is_in_charset(data->map[(int)(data->p_pos.r_y)][(int)(data->p_pos.r_x
+		+ data->p_pos.dir_x * move_speed)], "1D"))
+		data->p_pos.r_x += data->p_pos.dir_x * move_speed;
+	if (!ft_is_in_charset(data->map[(int)(data->p_pos.r_y + data->p_pos.dir_y
+		* move_speed)][(int)(data->p_pos.r_x)], "1D"))
         data->p_pos.r_y += data->p_pos.dir_y * move_speed;
 }
 
 
 void	behind(t_map_data *data, double move_speed)
 {
-	if (data->map[(int)(data->p_pos.r_y)][(int)(data->p_pos.r_x
-			- data->p_pos.dir_x * move_speed)] != '1'
-		&& data->map[(int)(data->p_pos.r_y)][(int)(data->p_pos.r_x
-			- data->p_pos.dir_x * move_speed)] != 'D')
+	if (!ft_is_in_charset(data->map[(int)(data->p_pos.r_y)][(int)(data->p_pos.r_x
+			- data->p_pos.dir_x * move_speed)], "1D"))
 		data->p_pos.r_x -= data->p_pos.dir_x * move_speed;
-	if (data->map[(int)(data->p_pos.r_y - data->p_pos.dir_y
-			* move_speed)][(int)(data->p_pos.r_x)] != '1'
-		&& data->map[(int)(data->p_pos.r_y - data->p_pos.dir_y
-			* move_speed)][(int)(data->p_pos.r_x)] != 'D')
+	if (!ft_is_in_charset(data->map[(int)(data->p_pos.r_y - data->p_pos.dir_y
+			* move_speed)][(int)(data->p_pos.r_x)], "1D"))
 		data->p_pos.r_y -= data->p_pos.dir_y * move_speed;
 }
